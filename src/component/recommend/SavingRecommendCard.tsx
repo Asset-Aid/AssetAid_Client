a304a2f9bfd45a59b8647ce317182ef73c920bab
psm1st <145013061+psm1st@users.noreply.github.com>
2024-11-18 01:10:34 +0900

    feat : saving 추가

--- src/component/recommend/SavingRecommendCard.tsx
+++ src/component/recommend/SavingRecommendCard.tsx
@@ -1,32 +1,31 @@
 import React from "react";
 import styled from "styled-components";
 
-interface DepositData {
-  depositId: number;
-  depositName: string;
+interface SavingData {
+  savingId: number;
+  savingName: string;
   bankColor: string;
   bankName: string;
 }
 
-interface DepositRecommendCardProps {
-  depositData: DepositData;
+interface SavingRecommendCardProps {
+  savingData: SavingData;
 }
 
-const DepositRecommendCard: React.FC<DepositRecommendCardProps> = ({
-  depositData,
+const SavingRecommendCard: React.FC<SavingRecommendCardProps> = ({
+  savingData,
 }) => {
   return (
-    <Card style={{ backgroundColor: depositData.bankColor }}>
-      <Title>{depositData.depositName}</Title>
+    <Card style={{ backgroundColor: savingData.bankColor }}>
+      <Title>{savingData.savingName}</Title>
       <ImageContainer>
-        <CardImage src="/assets/character1.png" alt="Character" />
+        <Character src="/assets/character1.png" alt="Character" />
       </ImageContainer>
-      <BankName>{depositData.bankName}</BankName>
+      <BankName>{savingData.bankName}</BankName>
     </Card>
   );
 };
 
-// Styled Components
 const Card = styled.div`
   width: 140px;
   height: 140px;
@@ -52,7 +51,7 @@ const ImageContainer = styled.div`
   right: 80px;
 `;
 
-const CardImage = styled.img`
+const Character = styled.img`
   width: 50px;
   height: 50px;
 `;
@@ -66,4 +65,4 @@ const BankName = styled.span`
   left: 78px;
 `;
 
-export default DepositRecommendCard;
+export default SavingRecommendCard;
